8589345c287976382dc524b9cc043c7988c74b75
marimragab <maryamragab@outlook.sa>
2023-12-27 12:38:33 +0200

    adding chat window

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit {
-  userId: number;
-  messages: any[] = [];
+  @Input() userId: string; // User ID for the chat window
 
-  constructor() {}
+  messages: string[] = [];
+  newMessage: string = '';
+  senderImage: string = '../../../assets/images/user.avif';
+  senderName: string = 'Mariam';
+  receiverImage: string = '../../../assets/images/user.avif';
+  receiverName: string = 'Heba';
+  hasPreviousMessages: boolean;
 
-  ngOnInit(): void {}
+  constructor(private chatService: ChatService) {}
+
+  ngOnInit() {
+    this.hasPreviousMessages = this.messages.length > 0;
+
+    // Initialize the chat and load initial messages
+    // this.chatService.initChat(this.userId);
+    // this.loadMessages();
+    // Subscribe to incoming messages
+    // this.chatService.messageReceived.subscribe((message: string) => {
+    //   this.messages.push(message);
+    // });
+  }
+
+  loadMessages() {
+    // Logic to load messages from the service
+    //this.messages = this.chatService.getMessages(this.userId);
+  }
+
+  sendMessage() {
+    // Logic to send a message through the service
+    // this.chatService.sendMessage(this.userId, this.newMessage);
+    //this.newMessage = ''; // Clear the input after sending
+  }
 }
