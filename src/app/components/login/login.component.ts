1f2777cfca2efacde4d6b533c8c00fba61b6c973
marimragab <maryamragab@outlook.sa>
2023-12-28 08:55:58 +0200

    edit login according to identity

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,15 +9,26 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  loginModel: Login = { email: '', password: '' };
-
+  loginModel: Login = { username: '', password: '' };
+  decodedToken: any;
   constructor(private authService: AuthService, private router: Router) {}
 
   login() {
     this.authService.login(this.loginModel).subscribe(
       (response) => {
         console.log('Login successful:', response);
-        if (response.role === 'admin') {
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+
+          this.decodedToken = this.authService.decodeToken(response.token);
+          console.log('Decoded Token:', this.decodedToken);
+        }
+
+        const isAdmin =
+          this.decodedToken[
+            'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
+          ].includes('Admin');
+        if (isAdmin) {
           this.router.navigate(['/admin']);
         } else {
           this.router.navigate(['/user']);
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Login } from '../models/login';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'https://localhost:7055/api';
+  private apiUrl = 'https://localhost:44331/api/account';
 
   constructor(private http: HttpClient) {}
 
   login(loginModel: Login): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/login`, loginModel);
+  }
+
+  decodeToken(token: string): any {
+    try {
+      return jwtDecode(token);
+    } catch (error) {
+      console.error('Error decoding token:', error);
+      return null;
+    }
   }
 }
